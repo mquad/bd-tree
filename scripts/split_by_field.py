4626733763d1676d754b1947d646d7b845eac42c
massimo <massimo.quadrana@polimi.it>
2015-03-30 11:38:12 +0200

    fixed scripts

--- scripts/split_by_field.py
+++ scripts/split_by_field.py
@@ -24,6 +24,15 @@ def extract_keys(fpath, key_id=0, delim='\t'):
             keys.add(key)
     return keys
 
+def build_dict(fpath, key_id=0, delim='\t'):
+    fdict = dict()
+    with open(fpath, 'r') as infile:
+        for line in infile:
+            key = line.split(delim)[key_id]
+            if key not in fdict:
+                fdict[key] = []
+            fdict[key].append(line)
+    return fdict
 
 def split_by_keys(fpath, outdir, train_keys, test_keys, key_idx=0, delim='\t'):
     fname = path.split(fpath)[-1]
@@ -39,6 +48,31 @@ def split_by_keys(fpath, outdir, train_keys, test_keys, key_idx=0, delim='\t'):
         train_file.close()
         test_file.close()
 
+
+def write_split(fdict, fname, outdir, train_keys, test_keys):
+    with open(path.join(outdir, fname + '.train'), 'w') as train_file:
+        for key in train_keys:
+            train_file.write(''.join(fdict.get(key, [])))
+    with open(path.join(outdir, fname + '.test'), 'w') as test_file:
+        for key in test_keys:
+            test_file.write(''.join(fdict.get(key, [])))
+
+
+def split_dict(train_fpath, test_fpath, outdir, key_idx=0, sampling_perc=1, train_perc=0.75, delim='\t'):
+    train_dict = build_dict(train_fpath, key_idx, delim)
+    test_dict = build_dict(test_fpath, key_idx, delim)
+    # shuffle keys
+    keys_shuffled = np.asarray(train_dict.keys())
+    np.random.shuffle(keys_shuffled)
+    # sample keys
+    sample_size = int(len(keys_shuffled)*sampling_perc)
+    keys_sampled = keys_shuffled[:sample_size]
+    # split the sample into training and test keys
+    train_size = int(sample_size*train_perc)
+    train_keys = keys_sampled[:train_size]
+    test_keys = keys_sampled[train_size:]
+    write_split(train_dict, path.split(train_fpath)[-1], outdir, train_keys, test_keys)
+    write_split(test_dict, path.split(test_fpath)[-1], outdir, train_keys, test_keys)
 
 def split(train_fpath, test_fpath, outdir, key_idx=0, sampling_perc=1, train_perc=0.75, delim='\t'):
     keys = extract_keys(train_fpath, key_idx, delim)
@@ -68,4 +102,4 @@ if __name__ == '__main__':
     parser.add_argument("--delim", dest="delim", default='\t', help="delimiter")
     args = vars(parser.parse_args())
     print args
-    split(**args)
+    split_dict(**args)
