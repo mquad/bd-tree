1dd28f0eb256f4a1cddc77ad4f8ed1aa5ecf18ae
massimo <massimo.quadrana@polimi.it>
2015-04-01 18:57:22 +0200

    added size hint for import2

--- src/main.cpp
+++ src/main.cpp
@@ -19,13 +19,13 @@ std::vector<rating_t> import(const std::string &filename){
     return ratings;
 }
 
-std::vector<rating_t> import2(const std::string &filename){
+std::vector<rating_t> import2(const std::string &filename, const std::size_t &size_hint=1000000){
     boost::iostreams::mapped_file mmap(filename, boost::iostreams::mapped_file::readonly);
     auto f = mmap.const_data();
     auto l = f + mmap.size();
 
     std::vector<rating_t> ratings;
-    ratings.reserve(10000000);
+    ratings.reserve(size_hint);
     bool ok = qi::phrase_parse(f,l,(qi::ulong_long > qi::ulong_long > qi::double_) % qi::eol, qi::blank, ratings);
     if (ok)
         std::cout << "parse success\n";
@@ -37,8 +37,8 @@ std::vector<rating_t> import2(const std::string &filename){
 
 int main(int argc, char **argv)
 {
-    if(argc < 10){
-        std::cout << "Usage: ./bd-tree <training-file> <validation-file> <query-file> <test-file> <lambda> <h-smooth> <max-depth> <min-ratings> <threads>" << std::endl;
+    if(argc < 11){
+        std::cout << "Usage: ./bd-tree <training-file> <validation-file> <query-file> <test-file> <lambda> <h-smooth> <max-depth> <min-ratings> <threads> <size-hint>" << std::endl;
         return 1;
     }
     std::string training_file(argv[1]);
@@ -50,6 +50,8 @@ int main(int argc, char **argv)
     unsigned max_depth = std::strtoul(argv[7], nullptr, 10);
     std::size_t min_ratings = std::strtoull(argv[8], nullptr, 10);
     unsigned num_threads = std::strtoul(argv[9], nullptr, 10);
+    std::size_t sz_hint = std::strtoull(argv[10], nullptr, 10);
+
 
 
     stopwatch sw;
@@ -57,19 +59,19 @@ int main(int argc, char **argv)
     sw.start();
     // build the decision tree
     BDTree bdtree{lambda, h_smoothing, num_threads};
-    bdtree.init(import2(training_file));
+    bdtree.init(import2(training_file, sz_hint));
     auto init_t = sw.elapsed_ms();
     std::cout << "Tree initialized in " << init_t / 1000.0 << " s." << std::endl ;
-//    bdtree.build(max_depth, min_ratings);
-//    std::cout << "Tree built in " << (sw.elapsed_ms() - init_t) / 1000.0 << " s." << std::endl ;
+    bdtree.build(max_depth, min_ratings);
+    std::cout << "Tree built in " << (sw.elapsed_ms() - init_t) / 1000.0 << " s." << std::endl ;
 
-//    user_profiles_t query_profiles, test_profiles;
-//    build_profiles(query_file, query_profiles);
-//    build_profiles(test_file, test_profiles);
-//    // evaluate tree quality
-//    double rmse_val = evaluate(bdtree, query_profiles, test_profiles, rmse);
-//    std::cout << "RMSE: " << rmse_val << std::endl;
-//    std::cout << "Process completed in " << sw.elapsed_ms() / 1000.0  << " s." << std::endl;
-//    return 0;
+    user_profiles_t query_profiles, test_profiles;
+    build_profiles(query_file, query_profiles);
+    build_profiles(test_file, test_profiles);
+    // evaluate tree quality
+    double rmse_val = evaluate(bdtree, query_profiles, test_profiles, rmse);
+    std::cout << "RMSE: " << rmse_val << std::endl;
+    std::cout << "Process completed in " << sw.elapsed_ms() / 1000.0  << " s." << std::endl;
+    return 0;
 }
 
