f7e7032a459d4985f973a8a08e73d4ef33fcbd31
massimo <massimo.quadrana@polimi.it>
2015-05-07 12:14:10 +0200

    added added_ratings

--- src/d_tree_eval.hpp
+++ src/d_tree_eval.hpp
@@ -30,6 +30,35 @@ double build_profiles(const std::string &filename, user_profiles_t &profiles){
     return sum / n;
 }
 
+
+template<typename T>
+std::vector<double> added_ratings(const T &dtree,
+                                   user_profiles_t &answers,
+                                   user_profiles_t &test){
+    std::vector<double> metric_avg(dtree.depth_max(), .0);
+    std::vector<int> counts(dtree.depth_max(), 0);
+    for(const auto &ans : answers){
+        if(test.count(ans.first) > 0){
+            unsigned level{0u};
+            auto node = dtree.root();
+            while(!node->is_leaf()){
+                int added = dtree.traverse(node, ans.second);
+                metric_avg[level] += static_cast<double>(added);
+                ++counts[level];
+                ++level;
+            }
+        }
+    }
+    std::transform(metric_avg.begin(), metric_avg.end(),
+                   counts.begin(), metric_avg.begin(),
+                   [](const double &sum, const int &count){return count > 0 ? sum / count : -1;});
+    // remove trailing elements
+    auto it = metric_avg.begin();
+    while(it != metric_avg.end() && *it != -1) ++it;
+    metric_avg.resize(std::distance(metric_avg.begin(), it));
+    return metric_avg;
+}
+
 template<typename T, typename Metric>
 std::vector<double> evaluate_error(const T &dtree,
                                      user_profiles_t &answers,
@@ -48,7 +77,7 @@ std::vector<double> evaluate_error(const T &dtree,
                     metric_avg[level] += metric;
                     ++counts[level];
                 }
-                node = dtree.traverse(node, ans.second);
+                dtree.traverse(node, ans.second);
                 ++level;
             }
         }
@@ -77,7 +106,7 @@ std::vector<double> evaluate_ranking(const T &dtree,
             while(node != nullptr){
                 metric_avg[level] += Metric::eval(dtree.ranking(node, test_ids), test.at(ans.first));
                 ++counts[level];
-                node = dtree.traverse(node, ans.second);
+                dtree.traverse(node, ans.second);
                 ++level;
             }
         }
