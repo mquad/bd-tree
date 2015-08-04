ca6a1625fc284b1f1ff2924fb2ad5a244ccd983c
massimo <max.square@gmail.com>
2015-08-04 17:03:33 +0200

    test without candidates

--- test/py_test.py
+++ test/py_test.py
@@ -11,7 +11,7 @@ density = .0001
 tree = ErrorTree(ratings_min=1, num_threads=4, depth_max=4, randomize=True, cache_enabled=True)
 tdata = [(int(random()*nusers), int(random()*nitems), int(random()*4+1)) for i in xrange(0, int(nusers*nitems*density))]
 tree.init(tdata)
-tree.build([i for i in xrange(0,500)], True)
+tree.build(True)
 
 traverser = ErrorTreeTraverser(tree)
 print traverser.current_query()
@@ -26,12 +26,10 @@ traverser.traverse_unknown()
 print traverser.current_query()
 print traverser.at_leaf()
 
-tree = RankNDCGTree(ratings_min=1, num_threads=4, depth_max=4, randomize=True, cache_enabled=True)
-tdata = [(int(random()*nusers), int(random()*nitems), int(random()*4+1)) for i in xrange(0, int(nusers*nitems*density))]
-tree.init(tdata)
+
 tree.build([i for i in xrange(0,500)], True)
 
-traverser = RankNDCGTreeTraverser(tree)
+traverser = ErrorTreeTraverser(tree)
 print traverser.current_query()
 print traverser.at_leaf()
 traverser.traverse_unknown()
@@ -43,4 +41,3 @@ print traverser.at_leaf()
 traverser.traverse_unknown()
 print traverser.current_query()
 print traverser.at_leaf()
-
